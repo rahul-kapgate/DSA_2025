5573004d599445a904da6cdf81b3ac9a8c3e07b3
rahul-kapgate <rahulkapgateyt@gmail.com>
2025-03-04 22:35:43 +0530

    Enhance Dictionary.py with examples of dictionary creation, modification, and access methods

--- 01 Python/02 Chai Aur code/data types/Dictionary.py
+++ 01 Python/02 Chai Aur code/data types/Dictionary.py
@@ -8,3 +8,43 @@
 # Mutable → Values can be changed, added, or removed.
 # Keys Must Be Unique → Duplicate keys are not allowed.
 # Keys Must Be Immutable → Keys can be str, int, tuple, etc., but not lists or dictionaries.
+
+# A dictionary in Python is an unordered collection of key-value pairs. 
+# It is defined using curly braces {} and allows fast lookups, insertions, and deletions.
+
+my_dict = {}    #empty dictionary
+
+person = {
+    "name" : "Rahul",
+    "age": 23,
+    "city" : "Bangalore"
+}
+
+print(person)
+
+## accsessing Dictionary values
+print(person["name"])
+print(person.get("age"))  #get() is safer because it returns None instead of an error if the key does not exist.
+
+# modifying dictionary
+
+# Updating values  
+person["name"] = "Rahul Kapgate"
+
+#add new key-value pair
+person["job"] = "Software Engineer"
+
+print(person)
+
+
+## Removing data
+del person["age"]   #delete a key
+print(person)
+
+name = person.pop("name") #remove and return value
+print(name)
+
+
+# Looping 
+for key, value in person.items():
+    print(key, "::", value)
